5b8b1214c43916ea636ec7f174c24723f47c7229
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-12-20 15:56:23 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,9 +15,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/api/dogs',dogsRouter);
 app.use('/api/cats',catsRouter);
