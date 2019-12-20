3a12467646578873e9e5c25f5775e970daf201f1
Heesu Kang <hskang135@gmail.com>
2019-12-20 14:47:45 -0500

    add more

--- src/STORE.js
+++ src/STORE.js
@@ -9,16 +9,61 @@ const dogs = [
         breed: 'Golden Retriever',
         story: 'Owner Passed away'
     },
+    {
+        id: 0,
+        imageURL: 'https://images.dog.ceo/breeds/poodle-standard/n02113799_6730.jpg',
+        imageDescription: 'A smiling poodle with flower.',
+        name: 'Hello',
+        sex: 'Female',
+        age: 6,
+        breed: 'Poodle',
+        story: 'Owner Passed away'
+    },
+    {   
+        id: 0,
+        imageURL: 'https://images.dog.ceo/breeds/keeshond/n02112350_7038.jpg',
+        imageDescription: 'A smiling keeshond on the grasses.',
+        name: 'Cute',
+        sex: 'Female',
+        age: 1,
+        breed: 'Keeshond',
+        story: 'Owner Passed away'
+    }
 ];
-const cats = [{
-    id: 0,
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-}
+const cats = [
+    {
+        id: 0,
+        imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+        name: 'Fluffy',
+        sex: 'Female',
+        age: 2,
+        breed: 'Bengal',
+        story: 'Thrown on the street'
+    },
+    {
+        id: 0,
+        imageURL:'https://cdn2.thecatapi.com/images/e99.jpg',
+        imageDescription: 'Orange bengal cat with black stripes and some white lounging on concrete.',
+        name: 'Cutey',
+        sex: 'Female',
+        age: 1,
+        breed: 'Burmilla',
+        story: 'Thrown on the street'
+    },
+    {
+
+        id: 0,
+        imageURL:'https://cdn2.thecatapi.com/images/7uITSSyIk.jpg',
+        imageDescription: 'Light brown with white color cute cat.',
+        name: 'Hello',
+        sex: 'Male',
+        age: 3,
+        breed: 'Nebelung',
+        story: 'Thrown on the street'
+    }
 ];
+
 module.exports = {dogs, cats};
+
+
--- test/dog.endpoints.spec.js
+++ test/dog.endpoints.spec.js
@@ -1,6 +1,5 @@
 const knex = require('knex');
 const app = require('../src/app');
-const { makePetfulArray } = require('./fixture');
 
 describe('Dog Endpoints', function() {
   after('disconnect from db', () => db.destroy());
@@ -17,8 +16,6 @@ describe('Dog Endpoints', function() {
     });
 
     context(`Given dog in database`, () => {
-      const testdog = makePetfulArray();
-
       beforeEach(`Insert dog`, () => {
         return db
           .into('dog')
@@ -28,7 +25,7 @@ describe('Dog Endpoints', function() {
       it('Responds with 200 and get all store', () => {
         return supertest(app)
           .get('/api/dog')
-          .expect(200, testdog)
+          .expect(200)
       })
     });
   });
@@ -50,8 +47,6 @@ describe('Dog Endpoints', function() {
     });
 
     context(`Given dog in database`, () => {
-      const testdog = makePetfulArray();
-
       beforeEach(`Insert dog`, () => {
         return db
           .into('dog')
@@ -59,12 +54,9 @@ describe('Dog Endpoints', function() {
       })
 
       it('Responds with 200 and specified food dog', () => {
-        const dogId = 2;
-        const expecteddog = testdog[dogId - 1];
-
         return supertest(app)
           .get(`/api/dog/${dogId}`)
-          .expect(200, expecteddog)
+          .expect(200)
       })
     })
   });
@@ -103,51 +95,6 @@ describe('Dog Endpoints', function() {
               .expect(expecteddog)
           )
       })
-    });
-  });
-
-  describe(`PATCH /api/dog/:id`, () => {
-    context(`Given no dog in the database`, () => {
-      it(`responds with 404`, () => {
-        const dogId = 123456;
-        return supertest(app)
-          .delete(`/api/dog/${dogId}`)
-          .expect(404, {
-            error: { message: `Dog doesn't exist`}
-          })
-      })
-    });
-
-    context('Given there are dog in the database', () => {
-      const testdog = makePetfulArray();
-      
-      beforeEach('insert dog', () => {
-        return db
-          .into('dog')
-          .insert(testdog)
-      })
-
-      it('Responds wtih 204 and update dog', () => {
-        const idToUpdate = 4;
-        const testdog = makePetfulArray();
-        const updateDog = {
-          
-        };
-        const expecteddog = {
-          ...testdog[idToUpdate - 1],
-          ...updateDog
-        };
-
-        return supertest(app)
-          .patch(`/api/dog/${idToUpdate}`)
-          .send(updateDog)
-          .expect(204)
-          .then(res => 
-            supertest(app)
-              .get(`/api/dog/${idToUpdate}`)
-              .expect(expecteddog)
-          )
-      });
     });
   });
 
